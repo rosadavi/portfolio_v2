af6a0c9e06e3a5a9650275ce57b9f63e0db3c944
rosadavi <davi.rosa.ofc@gmail.com>
2026-09-04 13:18:44 -0300

    feat: add public hello endpoint and security config

---
+++ src/main/java/br/com/rosadavi/controller/Hello.java
@@ -0,0 +1,12 @@
+package br.com.rosadavi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World";
+    }
+}
